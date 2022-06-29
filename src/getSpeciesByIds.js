1d181a18f905eb787010fe2315fefe0e6ded6c3c
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-06-29 14:57:19 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  return ids.map((id) => species.find((element) => element.id === id));
 }
 
 module.exports = getSpeciesByIds;
