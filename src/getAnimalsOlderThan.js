d6031e4d2fd0283448032668106da564793251d4
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-06-29 15:15:41 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const animalMap = species.find((element) => element.name === animal);
+  const { residents } = animalMap;
+  return residents.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
