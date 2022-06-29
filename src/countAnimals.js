c456c0a04524a531436210aa9bc3d2a7989d2de9
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-06-29 17:05:29 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  if (!animal) {
+    const createObj = {};
+    species.forEach((element) => {
+      createObj[element.name] = element.residents.length;
+    });
+    return createObj;
+  }
+  const value = Object.values(animal);
+  const quantity = Object.entries(animal).length;
+  if (quantity === 1) {
+    return species.find((element) => element.name === value[0]).residents.length;
+  }
+  if (quantity === 2) {
+    return species.find((element) => element.name === value[0])
+      .residents.filter((elemento) => elemento.sex === value[1]).length;
+  }
+}
+console.log(countAnimals({ specie: 'penguins' }));
 module.exports = countAnimals;
