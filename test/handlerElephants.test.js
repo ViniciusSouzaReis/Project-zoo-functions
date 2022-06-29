1be2aaa5a770b9edff43a20d37b57f0d8e3c2bf6
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-06-29 20:58:32 -0300

    Refatoranto Requisitos 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,37 +1,43 @@
-const { handlerElephants, computeData, averageAge, getElephants } = require('../src/handlerElephants');
+const { handlerElephants } = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it('Verifica se a função getElephants tem o retorno esperado', () => {
-    const expected = {
-      id: 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
-      name: 'elephants',
-      popularity: 5,
-      location: 'NW',
-      availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
-      residents: [
-        { name: 'Ilana', sex: 'female', age: 11 },
-        { name: 'Orval', sex: 'male', age: 15 },
-        { name: 'Bea', sex: 'female', age: 12 },
-        { name: 'Jefferson', sex: 'male', age: 4 },
-      ],
-    };
-    expect(getElephants()).toEqual(expected);
-  });
+  // it('Verifica se a função getElephants tem o retorno esperado', () => {
+  //   const expected = {
+  //     id: 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
+  //     name: 'elephants',
+  //     popularity: 5,
+  //     location: 'NW',
+  //     availability: ['Friday', 'Saturday', 'Sunday', 'Tuesday'],
+  //     residents: [
+  //       { name: 'Ilana', sex: 'female', age: 11 },
+  //       { name: 'Orval', sex: 'male', age: 15 },
+  //       { name: 'Bea', sex: 'female', age: 12 },
+  //       { name: 'Jefferson', sex: 'male', age: 4 },
+  //     ],
+  //   };
+  //   expect(getElephants()).toEqual(expected);
+  // });
 
-  it('Verifica se a função averageAge com o paramentro getElephants() retorna o valor 10.5', () => {
-    expect(averageAge(getElephants())).toBe(10.5);
-  });
+  // it('Verifica se a função averageAge com o paramentro getElephants() retorna o valor 10.5', () => {
+  //   expect(averageAge(getElephants())).toBe(10.5);
+  // });
 
-  it('Verifica o retorno da função computeData com diferentes parametros', () => {
-    expect(computeData('names', getElephants())).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
-    expect(computeData('count', getElephants())).toBe(4);
-    expect(computeData('averageAge', getElephants())).toBe(10.5);
-    expect(computeData()).toBeNull();
-  });
+  // it('Verifica o retorno da função computeData com diferentes parametros', () => {
+  //   expect(computeData('names', getElephants())).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  //   expect(computeData('count', getElephants())).toBe(4);
+  //   expect(computeData('averageAge', getElephants())).toBe(10.5);
+  //   expect(computeData()).toBeNull();
+  // });
 
   it('Verifica o retorno da função handlerElephants com diferentes parametros', () => {
     expect(handlerElephants()).toBeUndefined();
     expect(handlerElephants(4)).toEqual('Parâmetro inválido, é necessário uma string');
-    expect(handlerElephants('name')).toEqual('elephants');
+    expect(handlerElephants('a')).toBeNull();
+    expect(handlerElephants('count')).toEqual(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('location')).toEqual('NW');
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
   });
 });
