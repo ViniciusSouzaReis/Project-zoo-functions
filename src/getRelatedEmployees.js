cba0e08f32be3905cf8ce579730b5bec7ad3d1eb
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-06-29 16:10:31 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const { employees } = data;
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const { employees } = data;
+  const manager = employees.filter((element) => element.managers.includes(managerId));
+  const name = manager.map((element) => `${element.firstName} ${element.lastName}`);
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return name;
 }
 
 module.exports = { isManager, getRelatedEmployees };
