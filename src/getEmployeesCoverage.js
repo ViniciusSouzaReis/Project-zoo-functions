7b42b47192415b367937a98bd88c7e160dbb8911
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-06-30 16:54:42 -0300

    Arruma lint, continua

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -94,7 +94,3 @@ function getEmployeesCoverage(parameter) {
 }
 console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
 module.exports = getEmployeesCoverage;
-
-// else {
-//   throw new Error('Informações inválidas');
-// }
